0a873f9ea360e4eb6038311f5cc527ce3fb6aecb
sven4500 <sven4500@mail.ru>
2021-12-15 23:54:01 +0300

    Добавил промежуточный слой с активационной функцией гиперболический тангенс. Время обучения примерно 2 минуты.

--- lab-3.py
+++ lab-3.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 import tensorflow as tf
 from tensorflow import keras as keras
 import numpy as np
@@ -5,10 +11,17 @@ from matplotlib import pyplot as plt
 from timeit import default_timer as timer
 
 
+# In[ ]:
+
+
 # Целевая функция
 def fun(t):
     return np.cos(2.5 * t ** 2 - 5 * t)
     # return np.cos(-3 * t ** 2 + 5 * t + 10)
+
+
+# In[ ]:
+
 
 # 2.7.0
 print("TensorFlow version:", tf.__version__)
@@ -19,12 +32,12 @@ y = fun(t1)
 # Sequential API более простой чем Functional и позволяет работать только в
 # терминах слоёв.
 model = keras.models.Sequential()
-model.add(keras.Input(shape=(1,)))
-model.add(keras.layers.Dense(15, input_dim=1, activation='tanh'))
+model.add(keras.layers.Dense(40, input_dim=1, activation='tanh'))
     # kernel_initializer=keras.initializers.RandomNormal(stddev=0.5,mean=0.0),
     # bias_initializer=keras.initializers.RandomNormal(stddev=0.1,mean=0.0)))
     # bias_initializer=keras.initializers.Zeros()))
 # model.add(keras.layers.Dropout(0.05, input_shape=(dim,dim)))
+model.add(keras.layers.Dense(12, activation='tanh'))
 model.add(keras.layers.Dense(1, activation='linear'))
     # kernel_initializer=keras.initializers.RandomNormal(stddev=0.5,mean=0.0),
     # bias_initializer=keras.initializers.RandomNormal(stddev=0.1,mean=0.0)))
@@ -44,12 +57,14 @@ start = timer()
 hist = model.fit(t1, y, batch_size=batch_size, epochs=epochs)
 end = timer()
 
-print('Время на обучение:', end - start, 'сек.')
+
+# In[ ]:
+
 
 t2 = np.linspace(0, 3.5, 2000)
 out = model.predict(t2)
 
-print('Эпох:', epochs)
+print('Время на обучение:', (int)(end - start), 'сек.', 'Эпох:', epochs, 'Ошибка:', min(hist.history['mae']))
 
 plt.plot(t1, y)
 plt.plot(t2, out)
@@ -57,3 +72,10 @@ plt.plot(t2, out)
 plt.figure()
 plt.plot(hist.history['loss'])
 plt.plot(hist.history['mae'])
+
+
+# In[ ]:
+
+
+
+
