e5fc10954e4e8dd6af26ef19ebf0738572b93e9a
sven4500 <sven4500@mail.ru>
2021-12-15 23:41:43 +0300

    Оставил только два своя: гиперболический тангенс и линейная активационная функцмии.

--- lab-3.py
+++ lab-3.py
@@ -10,48 +10,50 @@ def fun(t):
     return np.cos(2.5 * t ** 2 - 5 * t)
     # return np.cos(-3 * t ** 2 + 5 * t + 10)
 
+# 2.7.0
+print("TensorFlow version:", tf.__version__)
 
-if __name__ == '__main__':
-    # 2.7.0
-    print("TensorFlow version:", tf.__version__)
+t1 = np.linspace(0, 3.4, 200)
+y = fun(t1)
 
-    t1 = np.linspace(0, 3.5, 250)
-    y = fun(t1)
+# Sequential API более простой чем Functional и позволяет работать только в
+# терминах слоёв.
+model = keras.models.Sequential()
+model.add(keras.Input(shape=(1,)))
+model.add(keras.layers.Dense(15, input_dim=1, activation='tanh'))
+    # kernel_initializer=keras.initializers.RandomNormal(stddev=0.5,mean=0.0),
+    # bias_initializer=keras.initializers.RandomNormal(stddev=0.1,mean=0.0)))
+    # bias_initializer=keras.initializers.Zeros()))
+# model.add(keras.layers.Dropout(0.05, input_shape=(dim,dim)))
+model.add(keras.layers.Dense(1, activation='linear'))
+    # kernel_initializer=keras.initializers.RandomNormal(stddev=0.5,mean=0.0),
+    # bias_initializer=keras.initializers.RandomNormal(stddev=0.1,mean=0.0)))
+    # bias_initializer=keras.initializers.Zeros()))
 
-    dim = 48
+# Вывести в консоль информацию о построенной модели
+model.summary()
 
-    # Sequential API более простой чем Functional и позволяет работать только в
-    # терминах слоёв.
-    model = keras.models.Sequential()
-    # model.add(keras.Input(shape=(1,)))
-    # model.add(keras.layers.Dropout(0.2, input_shape=(1,)))
-    model.add(keras.layers.Dense(dim, input_dim=1, activation='relu'))
-    model.add(keras.layers.Dense(dim, activation='relu'))
-    model.add(keras.layers.Dense(1, activation='sigmoid'))
+batch_size = 10
+epochs = 2000
 
-    # Вывести в консоль информацию о построенной модели
-    model.summary()
+# mse = mean square error, mae = mean absolute error
+model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+# model.compile(loss='mse', optimizer='adagrad', metrics=['mae'])
 
-    batch_size = 10
-    epochs = 1000
+start = timer()
+hist = model.fit(t1, y, batch_size=batch_size, epochs=epochs)
+end = timer()
 
-    # mse = mean square error, mae = mean absolute error
-    model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+print('Время на обучение:', end - start, 'сек.')
 
-    start = timer()
-    hist = model.fit(t1, y, batch_size=batch_size, epochs=epochs)
-    end = timer()
-    print('Время на обучение:', end - start)
+t2 = np.linspace(0, 3.5, 2000)
+out = model.predict(t2)
 
-    # out = model.predict([0.0])
-    t2 = np.linspace(0, 3.5, 1000)
-    out = model.predict(t2)
+print('Эпох:', epochs)
 
-    plt.plot(t1, y)
-    plt.plot(t2, out)
+plt.plot(t1, y)
+plt.plot(t2, out)
 
-    plt.figure()
-    plt.plot(hist.history['loss'])
-    plt.plot(hist.history['mae'])
-
-    x = []
+plt.figure()
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['mae'])
