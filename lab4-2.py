63d2f55d23fbc4f47cf01e516151159041aee3de
Ivars <sven4500@mail.ru>
2022-10-13 01:48:19 +0300

    Добавлено перемножение на квадратную матрицу перед выходом РБФ слоя.

--- lab4-2.py
+++ lab4-2.py
@@ -8,7 +8,7 @@ from keras import backend as back  # expand_dims, exp
 
 # Задать гиперпараметры обучения. В качестве размера пакета берём 10% от количества данных для обучения.
 batch_size = 15
-epochs = 175
+epochs = 5000
 
 mu_x, mu_y = [], []  # центры
 x_train = [0.0, 3.4]  # отрезок функции для обучения
@@ -22,22 +22,24 @@ class RBFLayer(Layer):
     def build(self, input_shape):
         # todo: keras рекомендует инициализировать веса в методе call, почему?
         self.mu = self.add_weight(name='mu',
-                                  # shape=(input_shape[1], self.output_dim),
                                   shape=(input_shape[1], self.output_dim,),
-                                  # initial_value=np.linspace(x_train[0], x_train[1], self.output_dim),
                                   initializer=keras.initializers.RandomUniform(minval=x_train[0], maxval=x_train[1]),
                                   trainable=True)
         self.sigma = self.add_weight(name='sigma',
                                      shape=(self.output_dim,),
-                                     initializer=keras.initializers.RandomNormal(stddev=0.05, mean=0.0),
+                                     initializer='random_normal',
                                      trainable=True)
+        self.sw = self.add_weight(name='sw',
+                                  shape=(self.output_dim,),
+                                  initializer='random_normal',
+                                  trainable=True)
         super(RBFLayer, self).build(input_shape)
 
     def call(self, inputs):
         global mu_x
         diff = back.expand_dims(inputs) - self.mu
         output = back.exp(back.sum(diff ** 2, axis=1) * self.sigma)
-        print(inputs.shape, diff.shape, output.shape, self.sigma.shape)
+        output = output * self.sw
         mu_x = back.eval(self.mu)
         return output
 
@@ -45,10 +47,10 @@ class RBFLayer(Layer):
 # Целевая функция, которую модель обучается предсказывать.
 def fun(t):
     # return np.cos(2.5 * t ** 2 - 5 * t)
-    # return np.sin(np.sin(t) * t ** 2)
+    return np.sin(np.sin(t) * t ** 2)
     # return np.cos(-3 * t ** 2 + 5 * t + 10)
     # return np.sin(0.25 * t ** 2 - 5 * t)
-    return np.sin(-np.sin(t) * t ** 2 + t)
+    # return np.sin(-np.sin(t) * t ** 2 + t)
     # return np.sin(t ** 2 - 6 * t + 3)
     # return t ** 2 - 6 * t + 3
 
@@ -60,7 +62,7 @@ def main():
     # Описать модель. Первый слой RBF, второй линейный. Инициализацию весов можно задать словами вместо чисел. По канону
     # выходной слой должен быть линейным. Здесь используем сигмоиду чтобы остаться в диапазоне [0...1].
     model = keras.models.Sequential()
-    model.add(RBFLayer(150, input_dim=1))
+    model.add(RBFLayer(15, input_dim=1))
     model.add(keras.layers.Dense(1, activation='linear',  # linear по канону
                                  kernel_initializer='random_normal',  # нормальное распределение (mean=0.0, stddev=0.05)
                                  bias_initializer='zeros'))  # равномерное распределение (minval=-0.05, maxval=0.05)
