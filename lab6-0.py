68335e6d0e817b850beec6b8b455cb1d07d15291
Ivars <sven4500@mail.ru>
2022-11-16 01:37:48 +0300

    Добавлена анимация.

--- lab6-0.py
+++ lab6-0.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.animation as animation
 from tqdm import tqdm
 
 
@@ -16,6 +17,8 @@ class SOM():
         dist = np.linalg.norm(self.nodes - input, axis=1)
         i_min = np.argmin(dist)
 
+        output = dist[i_min]
+
         # Найти расстояние от BMU до остальных узлов.
         dist = np.linalg.norm(self.indices - self.indices[i_min], axis=1)
 
@@ -24,6 +27,20 @@ class SOM():
             if d < radius:
                 influence = np.exp(-d / (2 * radius))
                 node += lr * influence * (input - node)
+
+        return output
+
+
+def update_fig(*args):
+    global im_axes
+    im_axes.set_array(frames[update_fig.counter])
+    update_fig.counter = (update_fig.counter + 1) % len(frames)
+    return im_axes,
+
+
+frames = []
+im_axes = 0
+update_fig.counter = 0
 
 
 def generate_train_data(num_samples):
@@ -50,6 +67,9 @@ def main():
     train_data = generate_train_data(num_samples)
     # train_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=1, shuffle=True)
 
+    global frames, im_axes
+    bmu_error = []
+
     for i in range(1, num_epochs):
         np.random.shuffle(train_data)
 
@@ -58,7 +78,9 @@ def main():
 
         for j, input in pbar:
             # Обновить карту.
-            model.update(input, radius, lr)
+            bmu_error += [model.update(input, radius, lr)]
+
+        frames += [np.copy(model.nodes).reshape(height, width, 3)]
 
         # Обновить радиус и скорость обучения в конце эпохи.
         radius = start_radius * np.exp(-i / (num_epochs / np.log(start_radius)))
@@ -69,14 +91,16 @@ def main():
     fig, axes = plt.subplots(2, 2)
     fig.tight_layout()
 
-    # axes[0, 0].set_title('Функция потерь')
-    # axes[0, 0].set_xlabel('Эпоха')
+    axes[0, 0].set_title('Функция потерь')
+    axes[0, 0].set_xlabel('Эпоха')
     # axes[0, 0].set_ylabel('MSE')
+    axes[0, 0].set_yscale('log')
+    axes[0, 0].plot(bmu_error)
 
-    # axes[1, 0].set_title('')
-    # axes[1, 0].set_aspect(1)
-    axes[1, 0].imshow(model.nodes.reshape((height, width, 3)))
+    axes[1, 0].set_title('')
+    im_axes = axes[1, 0].imshow(frames[0], animated=True)
 
+    anim = animation.FuncAnimation(fig, update_fig, interval=100, blit=True)
     plt.show()
 
 
